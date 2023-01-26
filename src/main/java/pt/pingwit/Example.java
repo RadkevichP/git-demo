6d20423db2fd8aeddbf410b23d8f53ea50accfef
Pavel Radkevich <pradkevich@eisgroup.com>
2023-01-26 09:10:37 +0200

    Initial commit

---
+++ src/main/java/pt/pingwit/Example.java
@@ -0,0 +1,12 @@
+package pt.pingwit;
+
+/**
+ * @author Pavel Radkevich
+ * @since 26.01.23
+ */
+public class Example {
+
+    public static void main(String[] args) {
+        System.out.println("Test");
+    }
+}
